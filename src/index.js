0d0edda72558d074d8200f7d281c578ca0a5d632
AnnaSamtash <sansara6900@gmail.com>
2024-03-09 17:22:00 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -14,3 +14,42 @@
  * прибрати завдання зі списку.
  * Список із завданнями має бути доступним після перезавантаження сторінки.
  */
+
+const form = document.querySelector('.header-form');
+const listOfNotes = document.querySelector('.tasks-list');
+const STORAGE_KEY = "notes";
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(event) {
+    event.preventDefault();
+    const nameTask = event.currentTarget.elements.taskName.value.trim();
+    const textTask = event.currentTarget.elements.taskText.value.trim();
+    console.log(nameTask, textTask);
+    if (nameTask === "" || textTask === "") {
+        alert("Please, fill all fields");
+        return;
+    }
+    const arrayData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [];
+    const taskObj = { nameTask, textTask, id : Date.now() };
+    arrayData.push(taskObj);
+    const jsn = JSON.stringify(arrayData);
+    localStorage.setItem(STORAGE_KEY, jsn);
+    event.currentTarget.reset();  
+    addToList();
+}
+
+addToList();
+
+
+
+function addToList() {
+    const arrayFromLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [];
+    const markup = arrayFromLocalStorage.map(({ nameTask, textTask, id }) => `<li class="task-list-item" data-id="${id}" >
+      <button class="task-list-item-btn">Видалити</button>
+      <h3>${nameTask}</h3>
+      <p>${textTask}</p>
+  </li>`
+    ).join("");
+    listOfNotes.innerHTML = markup;
+}
